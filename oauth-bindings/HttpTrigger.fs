0a88257d4a4766a352eae53ccd9dfcbc3557a67b
Dominik Raniszewski <dominik.raniszewski@gmail.com>
2020-08-21 22:57:10 +1000

    Unpacking user id from principal claims

--- oauth-bindings/HttpTrigger.fs
+++ oauth-bindings/HttpTrigger.fs
@@ -6,6 +6,7 @@ open Microsoft.Azure.WebJobs.Extensions.Http
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Logging
 open AccessTokenResult
+open System.Security.Claims
 
 // Using type notation for functions allows injections
 type HttpTrigger() =
@@ -14,13 +15,18 @@ type HttpTrigger() =
                 (log: ILogger)
                 ([<AccessToken>] accessToken) =
         async {
+
             log.LogInformation("F# HTTP trigger function processed a request.")
 
+            let unpackUserId (token: SuccessResult) = 
+                let userId  = token.Principal.Claims |> Seq.find (fun it -> it.Type = ClaimTypes.NameIdentifier) |> fun it -> it.Value
+                userId
+
             match accessToken with
-            | Success token -> return OkObjectResult(token) :> IActionResult
+            | Success token -> 
+                            let userId = unpackUserId token 
+                            return OkObjectResult("Hello " + userId) :> IActionResult
             | NoToken _ -> return UnauthorizedObjectResult("whaat") :> IActionResult
             | Error ex -> return UnauthorizedObjectResult(ex.Exception) :> IActionResult
             | Expired _ -> return BadRequestObjectResult("Expired") :> IActionResult
-            |  _ -> return BadRequestObjectResult("ascii shrug") :> IActionResult
-            
         } |> Async.StartAsTask
